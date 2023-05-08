7de73ac92721bcd9551b8167db4ab73b8669f967
Daiana Battista <dbattista.dg@gmail.com>
2023-05-07 23:50:39 -0300

    Entrega final desarrollo web

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*Video*/
+var video = document.getElementById("video__play");
+video
+function mute() {
+    if (video.muted === false) {
+        video.muted = true;
+    }
+    else {
+        video.muted = false;
+    }
+}
+document
+    .getElementById("sound__button")
+    .addEventListener("click", mute);
+
